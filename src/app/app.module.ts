b8e51347a6ff26a6ef72a21eae06056f2c7f8fe5
Emeka Uche <70174416+Emeka-Uche@users.noreply.github.com>
2020-09-30 09:16:52 +0100

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes} from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
